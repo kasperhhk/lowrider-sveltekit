ae0eb206258aa7e2bff896b88098987ecea5b958
kasperhhk <kkr@dis-play.dk>
2023-07-16 14:44:17 +0200

    add env var and override

--- src/routes/(authed)/+layout.server.ts
+++ src/routes/(authed)/+layout.server.ts
@@ -1,14 +1,16 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
+import { LOWRIDER_WEBSOCKET_HOST } from '$env/static/private';
 
-export const load: LayoutServerLoad = async ({ locals, url }) => {
+export const load: LayoutServerLoad = async ({ locals, url, route }) => {
   const { user } = locals;
   
   if (!user) {
     throw redirect(303, `/login?redirectTo=${url.pathname}`);
   }
 
-  const websocketServerHost = 'localhost:25565';
+  const useLocalhost = !!url.searchParams.get('uselocalhost');
+  const websocketServerHost = useLocalhost ? 'localhost:25565' : LOWRIDER_WEBSOCKET_HOST;
   const websocketServer = `wss://${websocketServerHost}/ws/`;
   return { username: user.username, websocketServer };
 };
