9c490a0acb7fa6fd194f46a9a9c589afc579d60a
kasperhhk <kkr@dis-play.dk>
2023-07-16 06:44:11 +0200

    force deploy

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -9,6 +9,7 @@ export const handle: Handle = async ({ event, resolve }) => {
     event.locals.session = getSession(sessionId);
   }
 
+  
   if (!event.locals.session) cookies.delete('sessionId');
 
   return await resolve(event);
