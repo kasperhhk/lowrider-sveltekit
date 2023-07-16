9b363902ba12d5a1ccbb38ee02e59833b2034050
kasperhhk <kkr@dis-play.dk>
2023-07-16 10:34:31 +0200

    fixup async usage in root

--- src/lib/server/jwt.ts
+++ src/lib/server/jwt.ts
@@ -3,8 +3,29 @@ import type { User } from './user';
 import { AUTH_RS256_PRIVATEKEY, AUTH_RS256_PUBLICKEY } from '$env/static/private';
 
 const alg = 'RS256';
-const privateKey = await jose.importPKCS8(AUTH_RS256_PRIVATEKEY, alg);
-const publicKey = await jose.importSPKI(AUTH_RS256_PUBLICKEY, alg);
+
+const keys = (function () {
+  let privateCache: jose.KeyLike | null = null;
+  const priv = async () => {
+    if (privateCache === null)
+      privateCache = await jose.importPKCS8(AUTH_RS256_PRIVATEKEY, alg);
+    return privateCache;
+  }
+
+  let publicCache: jose.KeyLike | null = null;
+  const pub = async () => {
+    if (publicCache === null)
+      publicCache = await jose.importSPKI(AUTH_RS256_PUBLICKEY, alg);
+    return publicCache;
+  };
+
+  return {
+    private: priv,
+    public: pub
+  };
+})();
+
+
 
 function mapToPayload(user: User) {
   return {
@@ -24,14 +45,14 @@ export async function createJwt(user: User) {
     .setProtectedHeader({ alg })
     .setIssuedAt()
     .setExpirationTime('1d')
-    .sign(privateKey);
+    .sign(await keys.private());
 
   return token;
 }
 
 export async function verifyJwt(token: string) {
   try {
-    const { payload } = await jose.jwtVerify(token, publicKey, {
+    const { payload } = await jose.jwtVerify(token, await keys.public(), {
       requiredClaims: ['lowrider_username'],
       maxTokenAge: '1d'
     });
