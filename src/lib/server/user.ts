f4f41fb1e53f4ecd596fd77334265f1d3ea09d02
kasperhhk <kkr@dis-play.dk>
2023-07-18 09:32:43 +0200

    add dev db bypass

--- src/lib/server/user.ts
+++ src/lib/server/user.ts
@@ -1,4 +1,5 @@
 import { sql } from '@vercel/postgres';
+import { dev } from '$app/environment';
 
 export interface User {
   username: string;
@@ -8,6 +9,12 @@ export async function MatchUserByPassword(username: string, password: string) {
   // Save db resources, i know what the usernames are pepelaff
   if (username !== 'kasper' && username !== 'ellie')
     return null;
+
+  if (dev) {
+    return {
+      username: 'kasper'
+    } as User;
+  }
 
   const result = await sql<User>`SELECT username FROM public.user u INNER JOIN public.user_auth ua ON ua.user_id = u.id WHERE u.username = ${username} AND ua.password = ${password} LIMIT 1`;
   return result.rowCount > 0 ? result.rows[0] : null;
