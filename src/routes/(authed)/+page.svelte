ef8e70edbce8df99c2abba64ffe56cf50c69ff98
kasperhhk <kkr@dis-play.dk>
2023-07-18 12:10:43 +0200

    add todos, fix autoscroll, fix enter submit

--- src/routes/(authed)/+page.svelte
+++ src/routes/(authed)/+page.svelte
@@ -1,26 +1,56 @@
 <script lang="ts">
-  import { onDestroy, onMount } from "svelte";
+  import { onDestroy, onMount, tick } from "svelte";
   import type { PageData } from "./$types";
   import { connect, disconnect } from "$lib/client/websocket/client";
   import { chatMessages } from "$lib/client/websocket/chat/stores";
   import { send } from "$lib/client/websocket/send";
 
+  //TODO: Make chat client as default export from chat lib instead of using websocket directly form component
+  //TODO: features should use a shared websocket underneath
+  //TODO: this also solves the typed "send" problem
+  //TODO: chat client should query history api -> store, then open websocket and put things into store from there
   export let data: PageData;
 
-  onMount(() => {
+  let chatMessagesDiv: HTMLDivElement;
+  let chatInputForm: HTMLFormElement;
+  let chatmsg = "";
+
+  onMount(async () => {
     connect(data.websocketServer, data.username);
+
+    await scrollChat();
+  });
+
+  const unsubscribe = chatMessages.subscribe(async _ => {
+    if (chatMessagesDiv) {
+      if (chatMessagesDiv.scrollTop + chatMessagesDiv.clientHeight === chatMessagesDiv.scrollHeight) {
+        await scrollChat();
+      }        
+    }
   });
 
   onDestroy(() => {
+    unsubscribe();
     disconnect();
   });
 
-  let chatmsg = "";
   function handleSubmit() {
     if (chatmsg) {
       send.chat.chatMessage({ message: chatmsg });
       chatmsg = "";
     }
+  }
+
+  function handleEnter(evt: KeyboardEvent) {
+    if (evt.key === "Enter" && !evt.shiftKey) {
+      evt.preventDefault();
+      handleSubmit();
+    }
+  }
+
+  async function scrollChat() {
+    await tick(); // wait for messages to be rendered
+    chatMessagesDiv.scrollTop = chatMessagesDiv.scrollHeight;
   }
 </script>
 
@@ -29,12 +59,12 @@
     <h1>Welcome {data.username}</h1>
   </main>
   <div class="chat-window">
-    <div class="chat-messages">
+    <div class="chat-messages" bind:this={chatMessagesDiv}>
       {#each $chatMessages as msg}
         <div>
           {#if msg.type === "CHAT:CHATMSG"}
             <span>{msg.timestamp.format("hh:mm")}</span>
-            <span>{msg.user}</span>
+            <span>{msg.user}:</span>
             <span>{msg.message}</span>
           {:else if msg.type === "chatclientmessage"}
             <span>{msg.timestamp.format("hh:mm")}</span>
@@ -44,9 +74,10 @@
       {/each}
     </div>
     <div class="chat-input">
-      <form on:submit|preventDefault={handleSubmit}>
+      <form on:submit|preventDefault={handleSubmit} bind:this={chatInputForm}>
         <textarea
           bind:value={chatmsg}
+          on:keypress={handleEnter}
           placeholder="Write something ..."
           autocomplete="off"
         />
